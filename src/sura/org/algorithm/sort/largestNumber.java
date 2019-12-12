ed18297c6c00ad32d8785fe8d118ffbba8b21a44
Leeseonbeen <seonbeen121@gmail.com>
2019-12-12 22:06:43 +0900

    largest

--- src/sura/org/algorithm/sort/largestNumber.java
+++ src/sura/org/algorithm/sort/largestNumber.java
@@ -1,25 +1,54 @@
 package sura.org.algorithm.sort;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class largestNumber {
 
-    public String solution(int[] numbers) {
-        String answer = "";
-        return answer;
+    public static void main(String[] args) {
+
+        int[] sol = {6,10,2};
+
+        largestNumber lg = new largestNumber();
+
+        String an = lg.solution(sol);
+        System.out.println(an);
+
+
     }
 
-    public int largeNumber(){
+    public String solution(int[] numbers) {
+        String answer = new String();
 
-        int[] numberArr = {6,10,2};
+        String str_number[] = new String[numbers.length];
 
-        for(int num : numberArr) {
+        for(int i=0; i<str_number.length; i++) {
+            str_number[i] = String.valueOf(numbers[i]);
+        }
 
+        Arrays.sort(str_number, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                System.out.println("처음수 : " + o2+o1);
+                System.out.println("비교 : " + o2+o1);
+                return (o2+o1).compareTo(o1+o2);
+            }
+        });
+
+        if(str_number[0].startsWith("0")) {
+            answer = "0";
+
+        } else {
+            for (String number: str_number) {
+                answer += number;
+            }
         }
 
 
 
-        return 0;
+        return answer;
     }
+
+
 
 }
