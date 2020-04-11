07339d431a90e5404407bd7b850aec33db90b4d1
Leeseonbeen <seonbeen121@gmail.com>
2020-04-11 14:35:01 +0900

    asd

---
+++ src/sura/org/algorithm/sort/hindex.java
@@ -0,0 +1,18 @@
+package sura.org.algorithm.sort;
+
+public class hindex {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int[] citations) {
+        int answer = 0;
+
+        /**
+         * n편 이상 h번 이상 인용
+         * h번 이하 인용 h-index
+         */
+        return answer;
+    }
+}
